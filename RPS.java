a71a749c0961c8f7f854893bd45005fcf7ca05e9
Muhammad Suleman <m.suleman275@gmail.com>
2022-06-17 17:30:22 +0300

    Create RPS.java

---
+++ RPS.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RPS {
+   public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Welcome to a game of Rock Paper Scissors\n");
+      System.out.println("How may rounds would you like to play?\n");
+      int rounds = scan.nextInt();
+   }
+}
